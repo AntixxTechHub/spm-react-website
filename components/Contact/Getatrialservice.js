f3c2ffa93115c7b6f371da356bb85ca31afa5840
AntixxTechHub <info@antixxtechhub.com>
2022-11-17 17:01:38 +0530

    Get a trail Services form popum

--- components/Contact/Getatrialservice.js
+++ components/Contact/Getatrialservice.js
@@ -7,6 +7,7 @@ const MySwal = withReactContent(Swal);
 import baseUrl from "@/utils/baseUrl";
 import GoogleMap from "./GoogleMap";
 import Link from "next/link";
+import gtstopimg from "@/public/images/gts_top_img.svg";
 var NewComponent = React.createClass
 
 
@@ -70,11 +71,8 @@ const ContactForm = () => {
       <div className="contact-area ptb-100">
         <div className="container">
         <div className="section-title style-two">
-            <span className="sub-title">Contact with us</span>
-            <h2>
-              Have Any Questions? <br />
-              Let&apos;s Talk!
-            </h2>
+            <span className="sub-title"></span>
+            <h2></h2>
           </div>
           <div className="row">
             <div className="col-lg-6 col-md-12">
@@ -149,54 +147,64 @@ const ContactForm = () => {
                     </div>
                     <div className="col-lg-12 col-md-12 col-sm-12">
                       <div className="form-group">
+                        <label>Trail Service On</label>
+                        <select className="form-control" id="00N2y000002FXr7" name="00N2y000002FXr7" title="Product Type">
+                                <option>--None--</option>
+                                <option value="IoT">IOT</option>
+                                <option value="Big Data">Big Data</option>
+                                <option value="Cyber Security & Cloud">Cyber Security and Cloud</option>
+                                <option value="Blockchain">Blockchain</option>
+                                <option value="Data Science">Data Science</option>
+                                <option value="BMC">BMC</option>
+                                <option value="Service Now">Service Now</option>
+                                <option value="Salesforce">Salesforce</option>
+                                <option value="AWS">AWS</option>
+                                <option value="SAP">SAP</option>
+                                <option value="Analytics">Analytics</option>
+                                <option value="Mainframe">Mainframe</option>
+                                <option value="ERP Application">ERP Application</option>
+                                <option value="Other">Other</option>
+                              </select>
+                      </div>
+                    </div>
+                    <div className="col-lg-12 col-md-12 col-sm-12">
+                      <div className="form-group">
                         <label>Message...</label>
                         <textarea
                           cols="30"
                           rows="6"
                           className="form-control"
                           id="00N2v00000XQu8f" name="00N2v00000XQu8f" type="text" wrap="soft" defaultValue={""}
-                          required onBlur={event => handleBlur('00N2v00000XQu8f')}
+                          required
+                          onBlur={event => handleBlur('00N2v00000XQu8f')}
                         ></textarea>
                       </div>
-                      <div style={{ display: 'none' }}>
-                        <label htmlFor="lead_source">lead Source</label>
-                        <input id="lead_source" maxLength={40} name="lead_source" size={20} type="text" defaultValue="Website" />
-                      </div>
-                    </div>
-                    <div className="col-lg-12 col-md-12 col-sm-12">
-                      <div className="form-check">
-                        <input
-                          type="checkbox"
-                          className="form-check-input"
-                          id="checkme"
-                        />
-                        <label className="form-check-label" htmlFor="checkme">
-                          Accept{" "}
-                          <Link href="/terms-conditions">
-                            <a>Terms of Services</a>
-                          </Link>{" "}
-                          and{" "}
-                          <Link href="/privacy-policy">
-                            <a>Privacy Policy</a>
-                          </Link>
-                        </label>
+                      <div style={{display: 'none'}}>
+                      <label for="Free_Trial_Service_Request_c">Free Trial Service Requestc</label>
+                      <input id="Free_Trial_Service_Request__c" maxLength={40} name="Free_Trial_Service_Request__c" size={20} type="text" defaultValue="True" />
                       </div>
                     </div>
                     <div className="col-lg-12 col-md-12 col-sm-12">
                     <label className="col-lg-12 col-md-12 col-sm-12" style={{ display: 'none' }} id="isShowErrorMsg">Invalid form value</label>
-                    <button id="btnSave"
+                       <button id="btnSave"
                         type="submit"
                         className="btn-style-one red-light-color"
                    onClick={()=>alertContent()}   >
                         Send Message
                       </button>
-                     </div>
+                      </div>
                   </div>
                 </form>
               </div>
             </div>
-            <div className="col-lg-6 col-md-12">
-              <GoogleMap />
+            <div
+              className="col-lg-6 col-md-12"
+              data-aos="fade-up"
+              data-aos-duration="1200"
+            >
+              <div className="goal-image style-two">
+                <Image src={gtstopimg} alt="" />
+              </div>
             </div>
           </div>
         </div>
