05b732b97b73feebafb07743a8219bcfd91d4d99
AntixxTechHub <info@antixxtechhub.com>
2022-12-19 16:49:37 +0530

    git push 19.12.22 base url

--- utils/baseApiUrl.js
+++ utils/baseApiUrl.js
@@ -2,11 +2,7 @@
 
 // const baseApiUrl = "https://api.spmglobaltech.com"
 
-<<<<<<< HEAD
 const baseApiUrl = "https://spm-strapi.herokuapp.com"
-=======
-const baseApiUrl = "https://spm-strapi.herokuapp.com/"
->>>>>>> c8e7ec3300dfbe1cd9b03ecca118ca92a10ac92d
 
 // process.env.NODE_ENV === "production" 
 // ? 'http://localhost:1337' 
