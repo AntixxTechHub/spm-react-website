c378d1e84783194b53f243e5a37ca72affce3dbe
SPM Global Technologies <118279637+InfoSPM@users.noreply.github.com>
2022-12-29 18:05:55 +0530

    1st image is changed with seo meta  alt

--- pages/bmc-smart-reporting.js
+++ pages/bmc-smart-reporting.js
@@ -7,6 +7,9 @@ import PageTopTitle from "@/components/Common/PageTopTitle";
 import SubscribeStyle1 from "@/components/Common/SubscribeStyle1";
 import FooterOne from "@/components/Layout/Footer/FooterOne";
 import Accordion from "@/components/Accordion";
+import baseApiUrl from "@/utils/baseApiUrl";
+import PageSeo from "../components/Common/PageSeo";
+import MediaImage from "../components/Common/Media";
 
 import SupportServices from "@/public/images/Support-Services.jpg";
 import Reportinfprocesses from "@/public/images/Reportinf-processes.png";
@@ -68,10 +71,11 @@ const questionsAnswers = [
 ];
 
 
-const Services = () => {
+const Services = ({ seo }) => {
+  
   return (
     <>
-      <PageTitle page="BMC Smart Reporting" />
+      <PageSeo seo={seo} pageName = "BMC Smart Reporting" />
       <Navbar />
       <PageTopTitle
         subTitle=""
@@ -98,7 +102,7 @@ const Services = () => {
               data-aos-duration="1200"
             >
               <div className="goal-image style-two">
-                <Image src={SupportServices} alt="" />
+              <MediaImage name="BMC Smart Reporting Support Services.jpg" data={seo} />
               </div>
             </div>
           </div>
@@ -538,5 +542,23 @@ const Services = () => {
     </>
   );
 };
+
+export async function getStaticProps({ params }) {
+  // console.log(params);
+  // Call an external API endpoint to get products.
+  // You can use any data fetching library
+  const res = await fetch(
+    `${baseApiUrl}/api/pages?filters[slug][$eq]=bmc-smart-reporting&populate=*`
+  );
+  const seo = await res.json();
+
+  // By returning { props: { blog } }, the Blog component
+  // will receive `blog` as a prop at build time
+  return {
+    props: {
+      seo,
+    },
+  };
+}
 
 export default Services;
