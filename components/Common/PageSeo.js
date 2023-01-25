e118e8e9c1b224217a2ffdb29c7149b63315f261
SPM Global Technologies <118279637+InfoSPM@users.noreply.github.com>
2023-01-25 13:53:03 +0530

    last line is removed

--- components/Common/PageSeo.js
+++ components/Common/PageSeo.js
@@ -3,7 +3,7 @@ import { NextSeo } from 'next-seo';
 const PageSeo = ({ seo, pageName }) => {
     const seoData = seo?.data?.length > 0 && seo?.data[0] && seo?.data[0].attributes?.seo ? seo?.data[0].attributes?.seo : {};
     return <NextSeo
-        title={seoData?.metaTitle || `${pageName} | SPM Global Technologies | `}
+        title={seoData?.metaTitle || `${pageName} | SPM Global Technologies `}
         description={seoData?.metaDescription}
         canonical={seoData?.canonicalURL}
         additionalMetaTags={[{
